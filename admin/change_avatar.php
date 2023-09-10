3857215eef98f47c933590840614d7320669289d
Nguyễn Thanh Liêm <105724928+ngtLiem@users.noreply.github.com>
2023-09-10 23:11:23 +0700

    update change avatar

--- admin/change_avatar.php
+++ admin/change_avatar.php
@@ -14,17 +14,17 @@
 -->
 <!DOCTYPE html>
 <html lang="en">
-<?php
-  session_start();
-?>
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="../assets/img/logo.png">
   <title>
-    Thay đổi mật khẩu
+    Thay đổi ảnh đại diện
   </title>
+  <?php
+    session_start();
+  ?>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
@@ -38,11 +38,11 @@
 </head>
 
 <body class="g-sidenav-show bg-gray-100">
-    <div class="position-absolute w-100 min-height-400 top-0" style="background-image: url('https://www.apple.com/lae/iphone-14-pro/images/key-features/hero/hero__cj6i78tzkp8i_large.jpg'); background-position-y: 50%;">
+<div class="position-absolute w-100 min-height-400 top-0" style="background-image: url('https://www.apple.com/lae/iphone-14-pro/images/key-features/hero/hero__cj6i78tzkp8i_large.jpg'); background-position-y: 50%;">
     <!-- <span class="mask bg-primary opacity-5"></span> -->
   </div>
   <?php
-    $active='tttk';
+    $active = 'tttk';
     require 'aside.php';
   ?>
   <div class="main-content position-relative max-height-vh-100 h-100">
@@ -52,21 +52,25 @@
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="dashboard.php">Trang</a></li>
-            <li class="breadcrumb-item text-sm text-white active" aria-current="page">Thay đổi mật khẩu</li>
+            <li class="breadcrumb-item text-sm text-white active" aria-current="page">Thay đổi ảnh đại diện</li>
           </ol>
-          <h6 class="font-weight-bolder text-white mb-0">Thay đổi mật khẩu</h6>
+          <h6 class="font-weight-bolder text-white mb-0">Thay đổi ảnh đại diện</h6>
         </nav>
         <!-- Đoạn này -->
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+            <div class="input-group">
+              <!-- <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
+              <input type="text" class="form-control" placeholder="Type here..."> -->
+            </div>
           </div>
           <ul class="navbar-nav  justify-content-end">
             <!-- <li class="nav-item d-flex align-items-center mb-4 me-4">
               <div class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
                 <img src="../assets/img/staff_img/<?php echo $_SESSION["avt"]; ?>" class="rounded-circle avatar avatar-xl" alt='user'>
               </div>
-            </li>
-            <li class="nav-item d-flex align-items-center mt-sm-1 ms-3">
+            </li> -->
+            <!-- <li class="nav-item d-flex align-items-center mt-sm-1 ms-3">
              <nav class=" mt-sm-1" aria-label="breadcrumb">   
                 <h7 class="text-white text mb-0">Xin chào,</h7>
                 <h6 class="font-weight-bolder text-white mt-n1"><?php echo $_SESSION["name"]; ?></h6>      
@@ -76,7 +80,7 @@
                 </a>
               </nav>
             </li>  -->
-
+            
             <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
               <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
                 <div class="sidenav-toggler-inner">
@@ -92,7 +96,52 @@
       </div>
     </nav>
     <!-- End Navbar -->
-    <div style="margin-top: 0; !important" class="card shadow-lg mx-4 card-profile-bottom"></div>
+    <div style="margin-top: 0; !important" class="card shadow-lg mx-4 card-profile-bottom">
+      <!-- <div class="card-body p-3">
+        <div class="row gx-4">
+          <div class="col-auto">
+            <div class="avatar avatar-xl position-relative">
+              <img src="../assets/img/team-1.jpg" alt="profile_image" class="w-100 border-radius-lg shadow-sm">
+            </div>
+          </div>
+          <div class="col-auto my-auto">
+            <div class="h-100">
+              <h5 class="mb-1">
+                Sayo Kravits
+              </h5>
+              <p class="mb-0 font-weight-bold text-sm">
+                Public Relations
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 my-sm-auto ms-sm-auto me-sm-0 mx-auto mt-3">
+            <div class="nav-wrapper position-relative end-0">
+              <ul class="nav nav-pills nav-fill p-1" role="tablist">
+                <li class="nav-item">
+                  <a class="nav-link mb-0 px-0 py-1 active d-flex align-items-center justify-content-center " data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="true">
+                    <i class="ni ni-app"></i>
+                    <span class="ms-2">App</span>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link mb-0 px-0 py-1 d-flex align-items-center justify-content-center " data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="false">
+                    <i class="ni ni-email-83"></i>
+                    <span class="ms-2">Messages</span>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link mb-0 px-0 py-1 d-flex align-items-center justify-content-center " data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="false">
+                    <i class="ni ni-settings-gear-65"></i>
+                    <span class="ms-2">Settings</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+     -->
+    </div>
     <div class="container-fluid py-4">
       <div class="row">
         <div class="col-md-8">
@@ -100,45 +149,70 @@
               <?php
 
                 $nvid = $_SESSION["nvid"];
-                $tkvaitro = $_SESSION["cv"];
+                $tkvaitro = $_SESSION["role"];
                 $tkavt = $_SESSION["avt"];
                 $nvhoten = $_SESSION["name"];
                 if($tkvaitro == '2'){
                     $tkvt = 'Nhân viên bán hàng';
-                  } else if ($tkvaitro == '3'){
+                  } else if($tkvaitro == '3'){
                     $tkvt = 'Nhân viên kế toán';
-                  } else{
+                  } else {
                     $tkvt = 'Quản lý';
                   } 
 
               ?>
           
-            <form action="update_change_password.php" method="post" enctype="multipart/form-data">
+            <form action="update_change_avatar.php" method="post" enctype="multipart/form-data">
               <div class="card-body">
-                <p class="text-uppercase text-sm mt-2">Thay đổi mật khẩu</p>
+                <p class="text-uppercase text-sm mt-2">Thay đổi ảnh đại diện</p>
                 <div class="row">
-                  <div class="col-md-12">
-                    <div class="form-group">
-                      <label for="example-text-input" class="form-control-label">Mật khẩu cũ</label>
-                      <input class="form-control" required type="password" name="oldpw">
-                    </div>
-                  </div>
-                  <div class="col-md-12">
-                    <div class="form-group">
-                      <label for="example-text-input" class="form-control-label">Mật khẩu mới</label>
-                      <input class="form-control" required type="password" name="newpw">
-                    </div>
-                  </div>
-                  <div class="col-md-12">
-                    <div class="form-group">
-                      <label for="example-text-input" class="form-control-label">Nhập lại mật khẩu mới</label>
-                      <input class="form-control" required type="password" name="renewpw">
-                    </div>
-                  </div>
-                  <div class="col-md-12">                                                            
-                      <div class="col-12 text-center">
-                        <button type="submit" class="btn btn-lg btn-outline-warning text-warning btn-lg w-100 mt-4 mb-0">Xác nhận thay đổi</button>
+                  <div class="col-12 card-header pb-2 d-flex align-items-center">
+                      <div class="col-2"></div>
+                      <div class="mb-3 px-3 col-4">
+                          Tải ảnh đại diện:
+                          <br>
+                          <input class="mt-3" type="file" name="staffImg" id="staffImg" accept="image/*">
+                          <input type="hidden" name="old_staffImg" value="<?php echo $tkavt;?>" accept="image/*">
                       </div>
+                      <div class="mb-3 px-3 col-4">
+                          <div class="ms-4" id="preview">
+                            <img id="old_img" src="../assets/img/staff_img/<?php echo $tkavt;?>" class="rounded-circle avatar avatar-xxl ms-4" alt="">
+                          </div>
+                          <script>
+                            var input = document.getElementById("staffImg");
+                            var preview = document.getElementById("preview");
+                            var old_img = document.getElementById("old_img");
+
+                            input.addEventListener("change", function() {
+                              preview.innerHTML = ""; // clear previous preview
+                              old_img.innerHTML = "";
+                              var files = this.files;
+                              for (var i = 0; i < files.length; i++) {
+                                var file = files[i];
+                                if (!file.type.startsWith("image/")){ continue } // skip non-image files
+                                var reader = new FileReader();
+                                reader.onload = function(e) {
+                                  var img = document.createElement("img");
+                                  img.src = e.target.result;
+                                  img.width = 300; // set width for preview images
+                                  img.className = "rounded-circle avatar avatar-xxl ms-4";
+                                  preview.appendChild(img); // append image to preview div
+                                };
+                                reader.readAsDataURL(file); // read file as data url
+                              }
+                            });
+                          </script>
+                      </div>
+                      <div class="col-2"></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 card-header pb-2 d-flex align-items-center">
+                        <div class="col-md-12">                                                            
+                            <div class="col-12 text-center">
+                                <button type="submit" class="btn btn-lg btn-primary text-white btn-lg w-100 mt-4 mb-0">Xác nhận thay đổi</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 
@@ -173,7 +247,7 @@
               <div class="row">
                 <div class="col">
                   <div class="d-flex justify-content-center">
-                  <div class="d-grid text-center">
+                    <div class="d-grid text-center">
                       <span class="text-lg font-weight-bolder">29</span>
                       <span class="text-sm opacity-8">Sản phẩm</span>
                     </div>
@@ -203,107 +277,9 @@
           </div>
         </div>
       </div>
-      <footer class="footer pt-3  ">
-        <div class="container-fluid">
-          <div class="row align-items-center justify-content-lg-between">
-            <div class="col-lg-6 mb-lg-0 mb-4">
-              <div class="copyright text-center text-sm text-muted text-lg-start">
-                © <script>
-                  document.write(new Date().getFullYear())
-                </script>,
-                made with <i class="fa fa-heart"></i> by
-                <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
-                for a better web.
-              </div>
-            </div>
-            <div class="col-lg-6">
-              <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
-                </li>
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted" target="_blank">About Us</a>
-                </li>
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
-                </li>
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </footer>
     </div>
   </div>
-  <div class="fixed-plugin">
-    <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
-      <i class="fa fa-cog py-2"> </i>
-    </a>
-    <div class="card shadow-lg">
-      <div class="card-header pb-0 pt-3 ">
-        <div class="float-start">
-          <h5 class="mt-3 mb-0">Argon Configurator</h5>
-          <p>See our dashboard options.</p>
-        </div>
-        <div class="float-end mt-4">
-          <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
-            <i class="fa fa-close"></i>
-          </button>
-        </div>
-        <!-- End Toggle Button -->
-      </div>
-      <hr class="horizontal dark my-1">
-      <div class="card-body pt-sm-3 pt-0 overflow-auto">
-        <!-- Sidebar Backgrounds -->
-        <div>
-          <h6 class="mb-0">Sidebar Colors</h6>
-        </div>
-        <a href="javascript:void(0)" class="switch-trigger background-color">
-          <div class="badge-colors my-2 text-start">
-            <span class="badge filter bg-gradient-primary active" data-color="primary" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-info" data-color="info" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-success" data-color="success" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
-          </div>
-        </a>
-        <!-- Sidenav Type -->
-        <div class="mt-3">
-          <h6 class="mb-0">Sidenav Type</h6>
-          <p class="text-sm">Choose between 2 different sidenav types.</p>
-        </div>
-        <div class="d-flex">
-          <button class="btn bg-gradient-primary w-100 px-3 mb-2 active me-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
-          <button class="btn bg-gradient-primary w-100 px-3 mb-2" data-class="bg-default" onclick="sidebarType(this)">Dark</button>
-        </div>
-        <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p>
-        <!-- Navbar Fixed -->
-        <hr class="horizontal dark my-sm-4">
-        <div class="mt-2 mb-5 d-flex">
-          <h6 class="mb-0">Light / Dark</h6>
-          <div class="form-check form-switch ps-0 ms-auto my-auto">
-            <input class="form-check-input mt-1 ms-auto" type="checkbox" id="dark-version" onclick="darkMode(this)">
-          </div>
-        </div>
-        <a class="btn bg-gradient-dark w-100" href="https://www.creative-tim.com/product/argon-dashboard">Free Download</a>
-        <a class="btn btn-outline-dark w-100" href="https://www.creative-tim.com/learning-lab/bootstrap/license/argon-dashboard">View documentation</a>
-        <div class="w-100 text-center">
-          <a class="github-button" href="https://github.com/creativetimofficial/argon-dashboard" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star creativetimofficial/argon-dashboard on GitHub">Star</a>
-          <h6 class="mt-3">Thank you for sharing!</h6>
-          <a href="https://twitter.com/intent/tweet?text=Check%20Argon%20Dashboard%20made%20by%20%40CreativeTim%20%23webdesign%20%23dashboard%20%23bootstrap5&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fargon-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
-            <i class="fab fa-twitter me-1" aria-hidden="true"></i> Tweet
-          </a>
-          <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/argon-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
-            <i class="fab fa-facebook-square me-1" aria-hidden="true"></i> Share
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!--   Core JS Files   -->
+  <<!--   Core JS Files   -->
   <script src="../assets/js/core/popper.min.js"></script>
   <script src="../assets/js/core/bootstrap.min.js"></script>
   <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
