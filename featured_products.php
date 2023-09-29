e435b5969f1a41563727e7affedf71fe1d339491
Nguyễn Thanh Liêm <105724928+ngtLiem@users.noreply.github.com>
2023-09-29 15:12:24 +0700

    update sản phẩm nổi bật

---
+++ featured_products.php
@@ -0,0 +1,69 @@
+<div class="row">
+    <!-- FEATURED-PRODUCTS-AREA START -->
+    <div class="featured-products-area">
+        <div class="center-title-area">
+            <h2 class="center-title">Sản phẩm nổi bật</h2>
+        </div>
+        <div class="col-xs-12">
+            <div class="row">
+                <!-- FEARTURED-CAROUSEL START -->
+                <div class="feartured-carousel">
+                    <!-- SINGLE-PRODUCT-ITEM START -->
+										<?php
+													require 'connect.php';
+													$sql = "select * from 
+																	chitiet_pn ct join san_pham sp on ct.SP_MA=sp.SP_MA 
+																	join phieu_nhap pn on ct.PN_STT=pn.PN_STT 
+																	ORDER BY pn.PN_NGAYNHAP";
+													
+													$result = $conn->query($sql);
+													if($result->num_rows>0){
+														while($row = $result->fetch_assoc()){
+													?>
+                    <div class="item">
+                        <div class="single-product-item">
+                            <div class="product-image">
+														<a href="product.php?id=<?php echo $row["SP_MA"] ?>"><img src="assets/img/product_img/<?php echo $row["SP_HINHANH"]?>" alt="product-image" /></a>
+                                <!-- <a href="#" class="new-mark-box">new</a> -->
+                                <div class="overlay-content">
+                                    <ul>
+                                        <li><a href="#" title="Quick view"><i class="fa fa-search"></i></a></li>
+                                        <li><a href="#" title="Quick view"><i class="fa fa-shopping-cart"></i></a></li>
+                                        <li><a href="#" title="Quick view"><i class="fa fa-retweet"></i></a></li>
+                                        <li><a href="#" title="Quick view"><i class="fa fa-heart-o"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="product-info">
+                                <div class="customar-comments-box">
+                                    <div class="rating-box">
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star-half-empty"></i>
+                                    </div>
+                                    <div class="review-box">
+                                        <span>1 Review (s)</span>
+                                    </div>
+                                </div>
+                                <a href="product.php?id=<?php echo $row["SP_MA"]?>"><?php echo $row["SP_TEN"]?></a>
+                                <div class="price-box">
+                                  <span class="price"><?php echo $row["SP_GIA"] ?> VNĐ</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+										<?php 
+											}
+										} 
+										?>
+                    <!-- SINGLE-PRODUCT-ITEM END -->
+                    <!-- SINGLE-PRODUCT-ITEM START -->
+                </div>
+                <!-- FEARTURED-CAROUSEL END -->
+            </div>
+        </div>
+    </div>
+    <!-- FEATURED-PRODUCTS-AREA END -->
+</div>
--- sale_products.php
+++ sale_products.php
@@ -1,27 +1,42 @@
-<!-- SALE-PRODUCTS START -->
+<!-----------------------------------------------------------------------------------------------------  -->
 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-    <div class="Sale-Products">
+    <!-- NEW-PRODUCT-AREA START -->
+    <div class="new-product-area">
         <div class="left-title-area">
             <h2 class="left-title">Sale Products</h2>
         </div>
         <div class="row">
             <div class="col-xs-12">
                 <div class="row">
-                    <!-- SALE-CAROUSEL START -->
-                    <div class="sale-carousel">
-                        <!-- SALE-PRODUCTS-SINGLE-ITEM START -->
+                    <!-- NEW-PRO-CAROUSEL START -->
+                    <div class="new-pro-carousel">
+                        <?php
+													require 'connect.php';
+													$sql = "select * from 
+																	chitiet_pn ct join san_pham sp on ct.SP_MA=sp.SP_MA 
+																	join phieu_nhap pn on ct.PN_STT=pn.PN_STT 
+																	ORDER BY pn.PN_NGAYNHAP";
+													
+													$result = $conn->query($sql);
+													if($result->num_rows>0){
+														while($row = $result->fetch_assoc()){
+													?>
+                        <!-- NEW-PRODUCT-SINGLE-ITEM START -->
                         <div class="item">
                             <div class="new-product">
                                 <div class="single-product-item">
                                     <div class="product-image">
-                                        <a href="#"><img src="img/product/sale/12.jpg" alt="product-image" /></a>
-                                        <a href="#" class="new-mark-box">new</a>
+                                        <a href="product.php?id=<?php echo $row["SP_MA"] ?>"><img
+                                                src="assets/img/product_img/<?php echo $row["SP_HINHANH"]?>"
+                                                alt="product-image" /></a> <a href="#" class="new-mark-box">sale!</a>
                                         <div class="overlay-content">
                                             <ul>
                                                 <li><a href="#" title="Quick view"><i class="fa fa-search"></i></a></li>
                                                 <li><a href="#" title="Quick view"><i class="fa fa-shopping-cart"></i></a></li>
-                                                <li><a href="#" title="Quick view"><i class="fa fa-retweet"></i></a></li>
-                                                <li><a href="#" title="Quick view"><i class="fa fa-heart-o"></i></a></li>
+                                                <li><a href="#" title="Quick view"><i class="fa fa-retweet"></i></a>
+                                                </li>
+                                                <li><a href="#" title="Quick view"><i class="fa fa-heart-o"></i></a>
+                                                </li>
                                             </ul>
                                         </div>
                                     </div>
@@ -31,28 +46,32 @@
                                                 <i class="fa fa-star"></i>
                                                 <i class="fa fa-star"></i>
                                                 <i class="fa fa-star"></i>
-                                                <i class="fa fa-star-half-empty"></i>
+                                                <i class="fa fa-star"></i>
                                                 <i class="fa fa-star-half-empty"></i>
                                             </div>
                                             <div class="review-box">
                                                 <span>1 Review (s)</span>
                                             </div>
                                         </div>
-                                        <a href="single-product.html">Printed Summer Dress</a>
+                                        <a href="product.php?id=<?php echo $row["SP_MA"]?>"><?php echo $row["SP_TEN"]?></a>
                                         <div class="price-box">
-                                            <span class="price">$28.98</span>
-                                            <span class="old-price">$30.51</span>
+                                            <span class="price"><?php echo $row["SP_GIA"] ?> VNĐ</span>
+																						<span class="old-price"><?php echo $row["SP_GIA"] ?> VNĐ</span>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <!-- SALE-PRODUCTS-SINGLE-ITEM END -->
+                        <?php 
+												}
+											}
+											?>
+                        <!-- NEW-PRODUCT-SINGLE-ITEM END -->
+                        <!-- NEW-PRODUCT-SINGLE-ITEM START -->
                     </div>
-                    <!-- SALE-CAROUSEL END -->
+                    <!-- NEW-PRO-CAROUSEL END -->
                 </div>
             </div>
         </div>
     </div>
 </div>
-<!-- SALE-PRODUCTS END -->
