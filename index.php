aa244a0186addc0e271bc1c72f215adab34c1e1a
Nguyễn Thanh Liêm <105724928+ngtLiem@users.noreply.github.com>
2023-10-04 22:16:39 +0700

    update head

--- index.php
+++ index.php
@@ -1,70 +1,7 @@
 <!doctype html>
 <html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Home</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Favicon
-		============================================ -->
-    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
-    <!-- FONTS
-		============================================ -->
-    <link href='http://fonts.googleapis.com/css?family=Oswald:400,700' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Bitter:400,700,400italic&amp;subset=latin,latin-ext'
-        rel='stylesheet' type='text/css'>
-    <!-- animate CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/animate.css">
-    <!-- FANCYBOX CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/jquery.fancybox.css">
-    <!-- BXSLIDER CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/jquery.bxslider.css">
-    <!-- MEANMENU CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/meanmenu.min.css">
-    <!-- JQUERY-UI-SLIDER CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/jquery-ui-slider.css">
-    <!-- NIVO SLIDER CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/nivo-slider.css">
-    <!-- OWL CAROUSEL CSS 	
-		============================================ -->
-    <link rel="stylesheet" href="css/owl.carousel.css">
-    <!-- OWL CAROUSEL THEME CSS 	
-		============================================ -->
-    <link rel="stylesheet" href="css/owl.theme.css">
-    <!-- BOOTSTRAP CSS 
-		============================================ -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!-- FONT AWESOME CSS 
-		============================================ -->
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <!-- NORMALIZE CSS 
-		============================================ -->
-    <link rel="stylesheet" href="css/normalize.css">
-    <!-- MAIN CSS 
-		============================================ -->
-    <link rel="stylesheet" href="css/main.css">
-    <!-- STYLE CSS 
-		============================================ -->
-    <link rel="stylesheet" href="style.css">
-    <!-- RESPONSIVE CSS 
-		============================================ -->
-    <link rel="stylesheet" href="css/responsive.css">
-    <!-- IE CSS 
-		============================================ -->
-    <link rel="stylesheet" href="css/ie.css">
-    <!-- MODERNIZR JS 
-		============================================ -->
-    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
-</head>
+<?php include "head.php" ?>
 
 <body>
     <?php
